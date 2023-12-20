e83e48ac00dd355cc88a51a8b28f5e3c55ec1e0a
Jan Ekiel <60410323+JanTadeuszEkiel@users.noreply.github.com>
2023-12-20 23:09:35 +0100

    czytam dane z microbit

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+let timestamp = 0
 basic.forever(function () {
-	
+    timestamp = Math.round(input.runningTime() / 1000)
+    serial.writeValue("czas pracy", timestamp)
+    basic.pause(100)
+    basic.pause(1000)
 })
